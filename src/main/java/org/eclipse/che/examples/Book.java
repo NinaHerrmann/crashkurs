87f342b976dbf3b18d8299269c8a156b0ee7cb22
ninaherrmann <nina.herrmann@gmx.de>
2021-02-20 07:44:03 +0000

    Added Book class

---
+++ src/main/java/org/eclipse/che/examples/Book.java
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+package org.eclipse.che.examples;
+
+public class Book {
+
+    int pages;
+
+    public Book(int pages) {
+        this.pages = pages;
+    }
+
+    public void setPages(int page) {
+        pages = page;
+    }
+
+    public int getPages()[
+        return pages;
+    ]
+}
